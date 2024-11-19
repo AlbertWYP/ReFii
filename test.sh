1680961ffda8c473b70f458264dc5521fca97167
AlbertWYP <102477620+AlbertWYP@users.noreply.github.com>
2024-11-19 16:28:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ eval "$(conda shell.bash hook)"
 conda activate dire
 
 EXP_NAME="diff_fe_sigma_2_fe"
-CKPT="/data1/yipeng_wei/DIRE_change/data/exp/diff_fe_sigma_2/ckpt/model_epoch_best.pth"
+CKPT="/data1/yipeng_wei/data/exp/diff_fe_sigma_2/ckpt/model_epoch_best.pth"
 DATASETS_TEST="diff_fe"
 python /data1/yipeng_wei/DIRE_change/test.py --gpus 2 --ckpt $CKPT --exp_name $EXP_NAME datasets_test $DATASETS_TEST 2>&1 | tee test.txt
