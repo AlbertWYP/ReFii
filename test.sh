3a46b8c5bf8bac9e20a5be05599a3887aa47b3bc
AlbertWYP <102477620+AlbertWYP@users.noreply.github.com>
2024-11-19 16:43:13 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 ### make sure that you have modified the EXP_NAME, CKPT, DATASETS_TEST
 
 eval "$(conda shell.bash hook)"
-conda activate dire
+conda activate refii
 
 EXP_NAME="diff_fe_sigma_2_fe"
 CKPT="/data1/yipeng_wei/data/exp/diff_fe_sigma_2/ckpt/model_epoch_best.pth"
 DATASETS_TEST="diff_fe"
-python /data1/yipeng_wei/DIRE_change/test.py --gpus 2 --ckpt $CKPT --exp_name $EXP_NAME datasets_test $DATASETS_TEST 2>&1 | tee test.txt
+python /data1/yipeng_wei/ReFii/test.py --gpus 2 --ckpt $CKPT --exp_name $EXP_NAME datasets_test $DATASETS_TEST 2>&1 | tee test.txt
